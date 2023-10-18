7fcf9ecc4f83cfa9092ef8b7c9d07445fc9f30b9
Marco Argentieri <3596602+tiero@users.noreply.github.com>
2023-10-18 04:17:45 +0200

    Add `serve` and `transcribe` sub-commands (#3)
    * feat: support 16bit WAV files with hound in case ffmpeg is not installed on the user system
    
    * format
    
    * Split into transcribe and serve commands
    
    * cargo fix
    
    * remove logs
    
    * implement serve command

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,45 @@
 #![warn(clippy::all, clippy::pedantic, clippy::nursery)]
 
 use clap::Parser;
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{header::CONTENT_TYPE, Body, Request, Response, Server, StatusCode};
+use multer::Multipart;
+use serde::Serialize;
+use serde_json::to_string;
+use std::fs;
 use std::path::Path;
-use utils::write_to;
+use std::{convert::Infallible, net::SocketAddr};
+use whisper_cli::{Language, Model, Size, Whisper};
+
+use crate::utils::write_to;
 
 mod utils;
 
-use whisper_cli::{Language, Model, Size, Whisper};
+#[derive(Serialize)]
+struct TranscriptionResponse {
+    text: String,
+}
 
 #[derive(Parser)]
-#[command(
-    author,
-    version,
-    about = "Locally transcribe audio files, using Whisper.",
-    long_about = "Generate a transcript of an audio file using the Whisper speech-to-text engine. The transcript will be saved as a .txt, .vtt, and .srt file in the same directory as the audio file."
-)]
-struct Args {
+struct Opts {
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+#[derive(Parser)]
+enum SubCommand {
+    #[command(about = "Start the transcription server.")]
+    Serve {
+        /// Port to listen on
+        #[clap(short, long, default_value = "3030")]
+        port: u16,
+    },
+    #[command(about = "Transcribe a given audio file.")]
+    Transcribe(TranscribeArgs),
+}
+
+#[derive(Parser)]
+struct TranscribeArgs {
     /// Name of the Whisper model to use
     #[clap(short, long, default_value = "medium")]
     model: Size,
@@ -25,6 +49,7 @@ struct Args {
     lang: Option<Language>,
 
     /// Path to the audio file to transcribe
+    #[clap(name = "AUDIO")]
     audio: String,
 
     /// Toggle translation
@@ -34,11 +59,120 @@ struct Args {
     /// Generate timestamps for each word
     #[clap(short, long, default_value = "false")]
     karaoke: bool,
+
+    /// Write transcription results to .txt, .vtt, and .srt files.
+    #[clap(short, long, default_value = "false")]
+    write: bool,
 }
 
 #[tokio::main]
 async fn main() {
-    let mut args = Args::parse();
+    let opts = Opts::parse();
+    match opts.subcmd {
+        SubCommand::Serve { port } => start_server(port).await,
+        SubCommand::Transcribe(args) => transcribe_audio(args).await,
+    }
+}
+
+async fn start_server(port: u16) {
+    let addr = SocketAddr::from(([127, 0, 0, 1], port));
+    let make_svc =
+        make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(handle_transcription)) });
+    let server = Server::bind(&addr).serve(make_svc);
+
+    println!("🏃‍♀️ Server running at: {}", addr);
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
+}
+
+// A handler for incoming requests.
+async fn handle_transcription(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    // Extract the `multipart/form-data` boundary from the headers.
+    let boundary = req
+        .headers()
+        .get(CONTENT_TYPE)
+        .and_then(|ct| ct.to_str().ok())
+        .and_then(|ct| multer::parse_boundary(ct).ok());
+
+    // Send `BAD_REQUEST` status if the content-type is not multipart/form-data.
+    if boundary.is_none() {
+        return Ok(Response::builder()
+            .status(StatusCode::BAD_REQUEST)
+            .body(Body::from("BAD REQUEST"))
+            .unwrap());
+    }
+
+    // Process the multipart e.g. you can store them in files.
+    let transcription_request = process_multipart(req.into_body(), boundary.unwrap()).await;
+
+    if let Ok(trans_req) = transcription_request {
+        let audio = Path::new(trans_req.as_str());
+        let mut whisper =
+            Whisper::new(Model::new(Size::TinyEnglish), Some(Language::English)).await;
+        let transcript = whisper.transcribe(audio, false, false).unwrap();
+        println!("time: {:?}", transcript.processing_time);
+
+        let transcript_text = transcript.as_text();
+        let response: TranscriptionResponse = TranscriptionResponse {
+            text: transcript_text,
+        };
+
+        let json_response = to_string(&response).expect("Failed to serialize to JSON");
+
+        return Ok(Response::new(Body::from(json_response)));
+    } else if let Err(err) = transcription_request {
+        return Ok(Response::builder()
+            .status(StatusCode::INTERNAL_SERVER_ERROR)
+            .body(Body::from(format!("INTERNAL SERVER ERROR: {}", err)))
+            .unwrap());
+    }
+
+    Ok(Response::new(Body::from("Success")))
+}
+
+// Process the request body as multipart/form-data.
+async fn process_multipart(body: Body, boundary: String) -> multer::Result<String> {
+    // Create a Multipart instance from the request body.
+    let mut multipart = Multipart::new(body, boundary);
+    let mut file_path = String::new();
+
+    // Iterate over the fields, `next_field` method will return the next field if
+    // available.
+    while let Some(mut field) = multipart.next_field().await? {
+        if field.name() == Some("file") {
+            // Get the field name.
+            let name = field.name();
+
+            // Get the field's filename if provided in "Content-Disposition" header.
+            let file_name = field.file_name();
+
+            // Get the "Content-Type" header as `mime::Mime` type.
+            let content_type = field.content_type();
+
+            println!(
+                "Name: {:?}, FileName: {:?}, Content-Type: {:?}",
+                name, file_name, content_type
+            );
+            // Process the field data chunks e.g. store them in a file.
+            let mut bytes_len = 0;
+            let mut audio_data = Vec::new();
+            while let Some(field_chunk) = field.chunk().await? {
+                audio_data.extend_from_slice(&field_chunk);
+                bytes_len += field_chunk.len();
+            }
+            println!("Bytes Length: {:?}", bytes_len);
+            let file_name_str: &str = field.file_name().as_ref().unwrap_or(&"audio.wav");
+            file_path = format!("/tmp/{}", file_name_str); // Adjust as necessary
+            fs::write(&file_path, audio_data).expect("Failed to write to file");
+            println!("Write the file to {}", file_path);
+        }
+    }
+
+    Ok(file_path)
+}
+
+async fn transcribe_audio(mut args: TranscribeArgs) {
     let audio = Path::new(&args.audio);
     let file_name = audio.file_name().unwrap().to_str().unwrap();
 
@@ -57,19 +191,23 @@ async fn main() {
     let transcript = whisper
         .transcribe(audio, args.translate, args.karaoke)
         .unwrap();
-
-    write_to(
-        audio.with_file_name(format!("{file_name}.txt")),
-        &transcript.as_text(),
-    );
-    write_to(
-        audio.with_file_name(format!("{file_name}.vtt")),
-        &transcript.as_vtt(),
-    );
-    write_to(
-        audio.with_file_name(format!("{file_name}.srt")),
-        &transcript.as_srt(),
-    );
-
     println!("time: {:?}", transcript.processing_time);
+
+    if args.write {
+        write_to(
+            audio.with_file_name(format!("{file_name}.txt")),
+            &transcript.as_text(),
+        );
+        write_to(
+            audio.with_file_name(format!("{file_name}.vtt")),
+            &transcript.as_vtt(),
+        );
+        write_to(
+            audio.with_file_name(format!("{file_name}.srt")),
+            &transcript.as_srt(),
+        );
+    } else {
+        println!("");
+        println!("🔊 {}", transcript.as_text());
+    }
 }
